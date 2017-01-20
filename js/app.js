f788f7b6553385db192ed3f1b92672b370fbce3f
Stuart Smith <stuartgordonsmith5@gmail.com>
2017-01-20 22:44:29 +0000

    created jquery animation for link icons and menu appear on link icon click

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 $(document).foundation();
+
+$("#link1, #link2, #link3, #link4").hide();
+
+$("#menu").on("click", function() {
+	$("#link1, #link2, #link3, #link4").toggle();
+	$("#menu").toggle();
+});
