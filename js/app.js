10bf93f4e7a5f8a4f35f20c4446365b2095bd07e
Stuart Smith <stuartgordonsmith5@gmail.com>
2017-01-21 01:21:39 +0000

    added slideToggle animation to menu opening

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(document).foundation();
 $("#link1, #link2, #link3, #close-btn").hide();
 
 $("#menu").on("click", function() {
-	$("#link1, #link2, #link3, #close-btn").toggle();
+	$("#link1, #link2, #link3, #close-btn").slideToggle("slow");
 	$("#menu").toggle();
 });
 
