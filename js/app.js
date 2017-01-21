7e9310ce50c238073b3c77a67077dfdf89f9ec35
Stuart Smith <stuartgordonsmith5@gmail.com>
2017-01-21 00:24:02 +0000

    added menu close button in html and js and styled in scss

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
 $(document).foundation();
 
-$("#link1, #link2, #link3, #link4").hide();
+$("#link1, #link2, #link3, #close-btn").hide();
 
 $("#menu").on("click", function() {
-	$("#link1, #link2, #link3, #link4").toggle();
+	$("#link1, #link2, #link3, #close-btn").toggle();
+	$("#menu").toggle();
+});
+
+$("#close-btn").on("click", function() {
+	$("#link1, #link2, #link3, #close-btn").toggle();
 	$("#menu").toggle();
 });
